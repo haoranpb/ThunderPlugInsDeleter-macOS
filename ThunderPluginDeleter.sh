24ce8a492d0a666bd7c8ac6869cc03e7b7b05013
ludanxer <ludanxer@gmail.com>
2018-09-24 22:34:32 +0800

    update for Thunder3.2.4

--- ThunderPluginDeleter.sh
+++ ThunderPluginDeleter.sh
@@ -1,12 +1,8 @@
 ##### 		Delete Useless Plugins		 #####
 cd /Applications/Thunder.app/Contents/PlugIns
 
-# If you have a VIP, remove the following line
 rm -rf *vip*.*
-
-# If you do not remove the following line, you may not be able to set the preferences again
-rm -rf app*.*
-
+rm -rf app*.* *browser*.* iOS*.* *manager.*
 rm -rf xlplayer.* ad*.* thunderstore.* f*.* xia*.* bb*.* sea*.*
 
 
